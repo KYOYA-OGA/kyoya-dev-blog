4a990a10e082e44fd6e7999f3baa8897ae6b0dcf
KYOYA OGASAWARA <kyoyaoga@gmail.com>
2021-11-17 14:55:25 +0900

    google analytics追加。lang属性修正。

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -18,9 +18,9 @@ const siteMetadata = {
   locale: 'ja',
   analytics: {
     // supports plausible, simpleAnalytics or googleAnalytics
-    plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    plausibleDataDomain: 'www.kyoya.dev/', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID, // e.g. UA-000000-2 or G-XXXXXXX
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,6 @@
 import { PageSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
 import { useForm } from 'react-hook-form'
-// import axios, { AxiosRequestConfig } from 'axios'
 import { ToastContainer, toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import { useGoogleReCaptcha } from 'react-google-recaptcha-v3'
@@ -24,14 +23,6 @@ export default function Contact() {
 
   const onSubmitForm = async (values) => {
     const reCaptchaToken = await executeRecaptcha('contactPage')
-    // const config: AxiosRequestConfig = {
-    //   method: 'POST',
-    //   url: `${process.env.NEXT_PUBLIC_API_URL}/api/contact`,
-    //   headers: {
-    //     'Content-Type': 'application/json',
-    //   },
-    //   data: { ...values, reCaptchaToken },
-    // }
     const data = { ...values, reCaptchaToken }
     try {
       const response = await fetch('/api/contact', {
